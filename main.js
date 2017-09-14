cc2eeb1717fa3c0cf2d549bbfde40e3e98e6f4f0
Michał <brakowski.m@gmail.com>
2017-09-14 19:42:40 +0200

    add functions that rotates the tetris elements

--- main.js
+++ main.js
@@ -77,6 +77,36 @@ function playerMove(dir) {
     }
 }
 
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while(colide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if(offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+function rotate(matrix, dir) {
+    for(let y = 0; y < matrix.length; ++y) {
+        for(let x = 0; x < y; ++x) {
+            [matrix[x][y], matrix[y][x]]
+                =
+            [matrix[y][x], matrix[x][y]]
+        }
+    }
+    if(dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -98,7 +128,7 @@ function update(time = 0) {
 const arena = createMatrix(12, 20);
 
 const player = {
-    pos: { x: 5, y: 5},
+    pos: { x: 5, y: 5 },
     matrix: matrix
 }
 
@@ -109,6 +139,10 @@ document.addEventListener('keydown', event => {
         playerMove(1);
     } else if(event.keyCode === 40) {
         playerDrop();
+    } else if(event.keyCode === 81) {
+        playerRotate(-1);
+    } else if(event.keyCode === 87) {
+        playerRotate(1);
     }
 });
 
