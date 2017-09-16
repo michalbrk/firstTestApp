e9f6ab7ea85c971aa22e8dda21403970f23ad61e
Michał <brakowski.m@gmail.com>
2017-09-16 13:31:40 +0200

    adding comments for the better functionality understanding

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 const canvas = document.getElementById("tetris");
+//Getting contex out
 const context = canvas.getContext('2d');
-
+//Scaling the game's block elements to be visible
 context.scale(20, 20);
 
 function arenaSweep() {
@@ -20,6 +21,8 @@ function arenaSweep() {
     }
 }
 
+//Checking if the arena has the cloumns and rows
+//and what is the value of the specific index (if 1 => there is a collision)
 function colide(arena, player) {
     const[m, o] = [player.matrix, player.pos];
     for(let y = 0; y < m.length; ++y) {
@@ -32,6 +35,8 @@ function colide(arena, player) {
     return false;
 }
 
+//Creating the matrix arrays which are blocks in the game
+//based on the given arguments
 function createMatrix(w, h) {
     const matrix = [];
     while(h--) {
@@ -86,6 +91,9 @@ function createPiece(type) {
     }
 }
 
+//Basis for the updating game's plot function
+//Most importantly drawing game's elements and clearing them
+//so that there is a movement efect
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0,0, canvas.width, canvas.height);
@@ -94,6 +102,7 @@ function draw() {
     drawMatrix(player.matrix, player.pos);
 }
     
+//Setting of the position of the game's blocks on the canvas
 function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((val, x) => {
@@ -105,6 +114,7 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+//Copying values from the player to the arena in the correct position
 function merge(arena, player) {
     player.matrix.forEach((row, y) => {
         row.forEach((val, x) => {
@@ -181,10 +191,14 @@ let dropInterval = 1000;
 
 let lastTime = 0;
 
+//Updating the creation and movement of the game's block
+//so that the game has the continuity
+//Counting and storing time of the movement of the elements
 function update(time = 0) {
     const deltaTime = time - lastTime;
     lastTime = time;
     
+    //Setting the game's element to drop every second
     dropCounter += deltaTime;
     if(dropCounter > dropInterval) {
         playerDrop();
@@ -211,12 +225,14 @@ const colors = [
 
 const arena = createMatrix(12, 20);
 
+//Setting the values of the specific game's block
 const player = {
     pos: { x: 0, y: 0 },
     matrix: null,
     score: 0
 }
 
+//Setting the keybord keys that are responsible for block movement
 document.addEventListener('keydown', event => {
     if(event.keyCode === 37) {
         playerMove(-1);
